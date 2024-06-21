f3603cd02ccf57297dd6a8eff8a277b54164b991
svnmurali-2004 <123376873+svnmurali-2004@users.noreply.github.com>
2024-06-21 11:08:44 +0530

    commit1

---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+
+import designerImage from '../assets/Designer.png'; // Ensure correct path here
+import designerImage2 from '../assets/Designer2.png'; // Ensure correct path here
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen " style={{ backgroundImage: `url(${designerImage2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      
+    </div>
+  );
+};
+
+export default Main;
---
+++ frontend/src/setupEnv.js
@@ -0,0 +1,5 @@
+
+
+// Load environment variables from .env file
+
+export const baseurl="http://localhost:5000"
